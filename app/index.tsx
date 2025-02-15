e5c6271ed893e0989d9dcda24609b7a2d36ed581
Marcap00 <cappellatomarco@yahoo.com>
2025-02-15 11:56:51 +0100

    added title

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
+
+
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+     style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text
+      style={styles.title}
+      >Counter App</Text>
+
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "fff",
+        },
+    title: {
+        fontSize: 24,
+        fontWeight: "bold"
+        }
+    })
