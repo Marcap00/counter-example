8203a5ea7e093249ecf4c77ea5ce24c482fce7af
Marcap00 <cappellatomarco@yahoo.com>
2025-02-15 17:09:24 +0100

    fixing

--- app/index.tsx
+++ app/index.tsx
@@ -5,21 +5,21 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Index() {
 
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-   useEffect(() => {
-       const getCount = async () => {
-           try {
-               const savedCount = await AsyncStorage.getItem("counter");
-               if (savedCount !== null) {
-                   setCount(parseInt(savedCount));
-               }
-           } catch (e) {
-          console.e("Error in retrieving the count:", error);
-          }
-       };
-       getCount();
-   }, []);
+    useEffect(() => {
+        const getCount = async () => {
+            try {
+                const savedCount = await AsyncStorage.getItem("counter");
+                if (savedCount !== null) {
+                    setCount(parseInt(savedCount));
+                }
+            } catch (e) {
+                console.error("Error in retrieving the count:", e);
+            }
+        };
+        getCount();
+    }, []);
 
 
     useEffect(() => {
@@ -27,40 +27,40 @@ export default function Index() {
             try {
                 await AsyncStorage.setItem("counter", count.toString());
             } catch (e) {
-                console.e("Error in saving the count:", error);
+                console.error("Error in saving the count:", e);
             }
         };
         saveCount();
     }, [count]);
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Counter App</Text>
-      <Text style={styles.counter}>{count}</Text>
-      <View style={styles.btnContainer}>
-        <Pressable
-        style={({ pressed }) => [
-            styles.btn,
-            styles.btnRed,
-            pressed && styles.btnPressed,
-        ]}
-        onPress={() => setCount(count - 1)}
-        >
-            <Text style={styles.btnText}>-</Text>
-        </Pressable>
-        <Pressable
-        style={({ pressed }) => [
-            styles.btn,
-            styles.btnGreen,
-            pressed && styles.btnPressed,
-        ]}
-        onPress={() => setCount(count + 1)}
-        >
-            <Text style={styles.btnText}>+</Text>
-        </Pressable>
-      </View>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Counter App</Text>
+            <Text style={styles.counter}>{count}</Text>
+            <View style={styles.btnContainer}>
+                <Pressable
+                    style={({ pressed }) => [
+                        styles.btn,
+                        styles.btnRed,
+                        pressed && styles.btnPressed,
+                    ]}
+                    onPress={() => setCount(count - 1)}
+                >
+                    <Text style={styles.btnText}>-</Text>
+                </Pressable>
+                <Pressable
+                    style={({ pressed }) => [
+                        styles.btn,
+                        styles.btnGreen,
+                        pressed && styles.btnPressed,
+                    ]}
+                    onPress={() => setCount(count + 1)}
+                >
+                    <Text style={styles.btnText}>+</Text>
+                </Pressable>
+            </View>
+        </View>
+    );
 }
 
 
@@ -70,21 +70,21 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center",
         backgroundColor: "#fff",
-        },
+    },
     title: {
         fontSize: 24,
         fontWeight: "bold",
         marginBottom: 15,
-        },
+    },
     counter: {
         fontSize: 32,
         fontWeight: "bold",
         marginBottom: 15,
-        },
+    },
     btnContainer: {
         flexDirection: "row",
         gap: 30
-        },
+    },
     btn: {
         width: 60,
         height: 60,
@@ -92,19 +92,26 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         borderRadius: 50,
-        },
+        // iOS
+        shadowColor: "#000", // Colore dell'ombra
+        shadowOffset: { width: 0, height: 4 }, // Offset orizzontale e verticale
+        shadowOpacity: 0.3, // Opacità dell'ombra
+        shadowRadius: 6, // Raggio di sfocatura dell'ombra
+        // Android
+        elevation: 5, // Elevazione per l'ombra su Android
+    },
     btnRed: {
         backgroundColor: "#FF3B30",
-        },
+    },
     btnGreen: {
         backgroundColor: "#4CD964",
-        },
+    },
     btnPressed: {
         opacity: 0.6,
-        },
+    },
     btnText: {
         fontSize: 24,
         color: "#fff",
         fontWeight: "bold",
-        },
+    },
 });
